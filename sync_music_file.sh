e580fb80c24dadf0c2f83f2616dbb33bce454b17
Gizing <673137011@qq.com>
2019-09-28 00:10:07 +0800

    增加同步音乐文件脚本：sync_music_file.sh

---
+++ sync_music_file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 将PC上指定目录的音乐文件同步到移动硬盘上，同名旧文件不覆盖
+
+if [ ! $1 ]; then
+    echo "usage: sh sync_music_file.bash xxx"
+    echo "xxx is any input what you like, it is useless"
+    exit
+fi
+
+# PC默认音乐路径
+default_music_path="/d/Download/Music/"
+#default_music_path="/d/Github-Project/test"
+
+# 移动硬盘默认音乐路径
+mobile_disk_music_path="/e/Music"
+#mobile_disk_music_path="/d/Github-Project/test2"
+
+awk -v src="$default_music_path" -v dst="$mobile_disk_music_path" 'BEGIN { cmd="cp -ri "src"/* "dst; print "n" | cmd; }'
