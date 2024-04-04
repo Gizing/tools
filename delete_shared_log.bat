5e740e71aa7e163a133c0081fd1cec5fdabfade1
Gizing <673137011@qq.com>
2024-04-04 16:48:51 +0800

    update delete_shared_log.bat

--- delete_shared_log.bat
+++ delete_shared_log.bat
@@ -1,4 +1,10 @@
 
-rem 定时删除 C:\ProgramData\USOShared\Logs 目录
+rem 定时删除各种目录
 echo on
 rd /s/q "C:\ProgramData\USOShared\Logs"
+rd /s/q "C:\Users\LiJieying\AppData\Local\CrashDumps"
+rd /s/q "C:\Users\LiJieying\AppData\Roaming\baidu\BaiduNetdisk\AutoUpdate\Download\MainApp"
+rd /s/q "C:\Users\LiJieying\AppData\Roaming\XYWinQT\app"
+rd /s/q "C:\Users\LiJieying\AppData\Local\Youdao\Ynote\CrashRpt"
+rd /s/q "C:\Users\LiJieying\AppData\Local\RailCrashReport"
+rd /s/q "C:\Users\LiJieying\AppData\Local\SquirrelTemp"
